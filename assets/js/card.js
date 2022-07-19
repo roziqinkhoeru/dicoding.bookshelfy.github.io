544a048520021963d6bcbe9d7de5ae83324e3a18
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2022-07-19 15:01:39 +0700

    Update all environment but not responsiveness

--- assets/js/card.js
+++ assets/js/card.js
@@ -13,7 +13,20 @@ const toggleDropdownSetting = (btn, menu) => {
   }
 };
 
+const closeDropdownSetting = (btn, menu) => {
+  for (let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener("click", () => {
+      for (let j = 0; j < menu.length; j++) {
+        menu[j].classList.remove("show");
+      }
+    });
+  }
+};
+
 // call function toggle show dropdown setting card book
 const dropdownCard = document.getElementsByClassName("dropdown_setting"),
-  btnSetting = document.getElementsByClassName("setting_book");
+  btnSetting = document.getElementsByClassName("setting_book"),
+  btnDropdownSetting = document.getElementsByClassName("btn_dropdown_setting");
+
 toggleDropdownSetting(btnSetting, dropdownCard);
+closeDropdownSetting(btnDropdownSetting, dropdownCard);
