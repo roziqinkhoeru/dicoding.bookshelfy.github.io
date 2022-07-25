1a8cf1e99da995806e9e1a062c4b5db913830990
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2022-07-25 22:23:50 +0700

    finish method to finished book

--- assets/js/book.js
+++ assets/js/book.js
@@ -68,70 +68,99 @@ const resetFormAddBook = () => {
 
 // method make bookshelf
 const makeBookshelf = (bookObject) => {
+  // object destructive
+  const { id, title, author, year, isFinished } = bookObject;
+
+  // create an element
   const cardBook = document.createElement("div"),
     cardBookBody = document.createElement("div"),
     cardBookBodyContent = document.createElement("div"),
     titleBook = document.createElement("p"),
     authorBook = document.createElement("p"),
     yearBook = document.createElement("p"),
-    buttonBook = document.createElement("div"),
-    btnDone = document.createElement("button"),
     settingBook = document.createElement("div"),
     dotThreeIcon = document.createElement("i"),
     dropdownSetting = document.createElement("div"),
     btnDelete = document.createElement("button"),
     trashIcon = document.createElement("i"),
-    btnDeleteValue = document.createElement("span"),
-    btnSwitch = document.createElement("button"),
-    switchIcon = document.createElement("i"),
-    btnSwitchValue = document.createElement("span"),
-    statusBook = document.createElement("div"),
-    statusBookText = document.createElement("p"),
-    statusIcon = document.createElement("i"),
-    statusBookValue = document.createElement("span");
+    btnDeleteValue = document.createElement("span");
 
+  // add class to element
   cardBook.classList.add("card_book");
   cardBookBody.classList.add("card_book_body");
   cardBookBodyContent.classList.add("card_book_body_content");
   titleBook.classList.add("title_book");
   authorBook.classList.add("author_book");
   yearBook.classList.add("year_book");
-  buttonBook.classList.add("button_book");
-  btnDone.classList.add("btn", "btn_submit");
   settingBook.classList.add("setting_book");
   dotThreeIcon.classList.add("fa-solid", "fa-ellipsis-vertical");
   dropdownSetting.classList.add("dropdown_setting");
   btnDelete.classList.add("btn", "btn_delete", "btn_dropdown_setting");
   trashIcon.classList.add("fa-solid", "fa-trash-can");
-  btnSwitch.classList.add("btn", "btn_switch", "btn_dropdown_setting");
-  switchIcon.classList.add("fa-solid", "fa-shuffle");
-  statusBook.classList.add("status_book");
-  statusIcon.classList.add("fa-solid", "fa-check-double");
 
-  titleBook.innerText = bookObject.title;
-  authorBook.innerText = bookObject.author;
-  yearBook.innerText = bookObject.year;
-  btnDone.innerText = "Selesai";
+  // add content to element
+  titleBook.innerText = title;
+  authorBook.innerText = author;
+  yearBook.innerText = year;
   btnDeleteValue.innerText = "Hapus";
-  btnSwitchValue.innerText = "Belum selesai";
-  statusBookValue.innerText = "Selesai";
 
+  // create a component
   cardBookBodyContent.append(titleBook, authorBook, yearBook);
-  buttonBook.append(btnDone);
-  statusBook.appendChild(statusBookText).append(statusIcon, statusBookValue);
   settingBook.append(dotThreeIcon);
   btnDelete.append(trashIcon, btnDeleteValue);
-  btnSwitch.append(switchIcon, btnSwitchValue);
-  if (bookObject.isFinished) {
+
+  if (isFinished) {
+    // create an element
+    const btnSwitch = document.createElement("button"),
+      switchIcon = document.createElement("i"),
+      btnSwitchValue = document.createElement("span"),
+      statusBook = document.createElement("div"),
+      statusBookText = document.createElement("p"),
+      statusIcon = document.createElement("i"),
+      statusBookValue = document.createElement("span");
+
+    // add class to element
+    btnSwitch.classList.add("btn", "btn_switch", "btn_dropdown_setting");
+    switchIcon.classList.add("fa-solid", "fa-shuffle");
+    statusBook.classList.add("status_book");
+    statusIcon.classList.add("fa-solid", "fa-check-double");
+
+    // add content to element
+    btnSwitchValue.innerText = "Belum selesai";
+    statusBookValue.innerText = "Selesai";
+
+    // create component
+    statusBook.appendChild(statusBookText).append(statusIcon, statusBookValue);
+    btnSwitch.append(switchIcon, btnSwitchValue);
     cardBookBody.append(cardBookBodyContent, statusBook);
     dropdownSetting.append(btnSwitch, btnDelete);
   } else {
+    // cretae an element
+    const buttonBook = document.createElement("div"),
+      btnDone = document.createElement("button");
+    // add class to element
+    buttonBook.classList.add("button_book");
+    btnDone.classList.add("btn", "btn_submit");
+    // add content to element
+    btnDone.innerText = "Selesai";
+    // create a component
+    buttonBook.append(btnDone);
     cardBookBody.append(cardBookBodyContent, buttonBook);
     dropdownSetting.append(btnDelete);
+
+    // move to finished event
+    btnDone.addEventListener("click", () => {
+      addBookToFinished(id);
+    });
   }
 
-  cardBook.setAttribute("id", `book-${bookObject.id}`);
+  cardBook.setAttribute("id", `book-${id}`);
   cardBook.append(cardBookBody, settingBook, dropdownSetting);
+
+  // event delete
+  btnDelete.addEventListener("click", () => {
+    // code
+  });
 
   return cardBook;
 };
@@ -168,6 +197,17 @@ const addBook = () => {
   );
 
   books.push(bookItem);
+
+  document.dispatchEvent(new Event(RENDER_EVENT));
+  saveData();
+};
+
+// add book to finished book
+const addBookToFinished = (idBook) => {
+  const bookTarget = findBook(idBook);
+  if (bookTarget == null) return;
+
+  bookTarget.isFinished = true;
 
   document.dispatchEvent(new Event(RENDER_EVENT));
   saveData();
